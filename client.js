6067f7405cf12a93669d1ce860d55ed788ec76f0
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-05 01:37:24 +0000

    added constant functionality

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
+
 /**
  * Establishes connection with the game server
  */
 const connect = function () {
   const conn = net.createConnection({
     // host: '192.168.0.139',
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
   conn.setEncoding('utf8');
   conn.on('connect', () => {
