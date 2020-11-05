62307a0ec1e3d83d0cac4722bf439ab29e19e2f8
Kashif Rizvee <rizveekashif4u@gmail.com>
2020-11-05 00:39:29 +0000

    added input and play file with the concept of seperation of modularity

---
+++ input.js
@@ -0,0 +1,25 @@
+const handleUserInput = function (stdin) {
+  stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    }
+    process.stdout.write('.');
+  });
+}
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin)  
+  return stdin;
+}
+
+
+module.exports = {setupInput}
